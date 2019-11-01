7a774bdc8085aceb1576f7b9f6890708ce7bf125
shunjid <shunjid1862@gmail.com>
2019-11-01 21:03:27 +0600

    Designed Account Model

---
+++ src/Models/Account.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace src.Models
+{
+    public class Account
+    {
+        [Key]
+        [Required]
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string UserPhone { get; set; }
+        
+        public bool EmailConfirmed { get; set; }
+        public bool PhoneConfirmed { get; set; }
+        public bool TwoFaEnabled { get; set; }
+        public int AccessFailed { get; set; }
+        public bool AccountLocked { get; set; }
+    }
+}
