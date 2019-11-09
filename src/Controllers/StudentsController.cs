19d1b44b8135b07d368f828e5c18dcae7b60ca30
jinnatul <morolswediu@gmail.com>
2019-11-09 17:03:10 +0600

    update color array elements

--- src/Controllers/StudentsController.cs
+++ src/Controllers/StudentsController.cs
@@ -1,5 +1,4 @@
 using System;
-using System.CodeDom.Compiler;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -7,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 using src.Models;
 
 namespace src.Controllers
@@ -35,164 +33,240 @@ namespace src.Controllers
                 var acc = _context.Account.Find(userId);
                 return acc.Role;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return null;
             }
         }
         
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.Serial == id);
+        }
+        
+        private string __getAuthorizationCommand(string sid)
+        {
+            var currentlyLoggedInUser = _getCurrentlyLoggedInUser();
+            
+            if (string.IsNullOrEmpty(currentlyLoggedInUser))
+            {
+                return "Login";
+            }
+
+            if (_getAccountRoleFromUserId(sid) != "Student")
+            {
+                return "Teacher";
+            }
+
+            if (sid != currentlyLoggedInUser)
+            {
+                return _getAccountRoleFromUserId(currentlyLoggedInUser) == "Teacher" ? "Teacher" : "Self";
+            }
+
+            return Regex.Replace(sid, @"\s+", "") == "" ? "Self" : "Continue";
+        }
+        
         
         public async Task<IActionResult> __init__(string sid)
         {
-            if (_getCurrentlyLoggedInUser() == "" || _getCurrentlyLoggedInUser() == null)
-            {
-                return RedirectToAction("LogIn", "Account");
-            }
+            var currentlyLoggedInUser = _getCurrentlyLoggedInUser();
+            var authCommand = __getAuthorizationCommand(sid);
             
-            if (_getAccountRoleFromUserId(sid) != "Student")
+            switch (authCommand)
             {
-                return RedirectToAction("__init__", "Teachers", new { tid = _getCurrentlyLoggedInUser() });
-            }
-
-            if (sid != _getCurrentlyLoggedInUser() || sid == null || Regex.Replace(sid, @"\s+", "") == "")
-            {
-                await __init__(_getCurrentlyLoggedInUser());
-            }
-
-            var student = await _context.Students
-                .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
-            
-            var institutionList = await _context.Institutions.ToListAsync();
-
-            foreach (var institution in institutionList)
-            {
-                student.Institutions.Add(new SelectListItem
+                case "Login":
+                    return RedirectToAction("LogIn", "Account");
+                case "Teacher":
+                    return RedirectToAction("__init__", "Teachers", new { tid = currentlyLoggedInUser });
+                case "Self":
+                    await __init__(currentlyLoggedInUser);
+                    break;
+                case "Continue":
                 {
-                    Value = institution.InstitutionId,
-                    Text = institution.InstitutionName
-                });
+                    var student = await _context.Students
+                        .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
+            
+                    var institutionList = await _context.Institutions.ToListAsync();
+
+                    foreach (var institution in institutionList)
+                    {
+                        student.Institutions.Add(new SelectListItem
+                        {
+                            Value = institution.InstitutionId,
+                            Text = institution.InstitutionName
+                        });
+                    }
+
+                    ViewBag.studentAccountId = sid;
+                    return View(student);
+                }
+                default:
+                    await __init__(currentlyLoggedInUser);
+                    break;
             }
 
-            ViewBag.studentAccountId = sid;
-            return View(student);
+            return RedirectToAction("Logout", "Account");
         }
         
         [HttpPost]
         public async Task<IActionResult> __init__(Student student)
         {
-            var institutionSelected =
-                await _context.Institutions.SingleOrDefaultAsync(i =>
-                    i.InstitutionId == student.Institution.InstitutionId);
+            var currentlyLoggedInUser = _getCurrentlyLoggedInUser();
+            var authCommand = __getAuthorizationCommand(currentlyLoggedInUser);
             
-            student.Institution = institutionSelected;
-            
-            if (!ModelState.IsValid) return View(student);
-            
-            try
+            switch (authCommand)
             {
-                _context.Update(student);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(student.Serial))
+                case "Login":
+                    return RedirectToAction("LogIn", "Account");
+                case "Teacher":
+                    return RedirectToAction("__init__", "Teachers", new { tid = currentlyLoggedInUser });
+                case "Self":
+                    await __init__(currentlyLoggedInUser);
+                    break;
+                case "Continue":
                 {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+                    var institutionSelected =
+                        await _context.Institutions.SingleOrDefaultAsync(i =>
+                            i.InstitutionId == student.Institution.InstitutionId);
+            
+                    student.Institution = institutionSelected;
+            
+                    if (!ModelState.IsValid) return View(student);
+            
+                    try
+                    {
+                        _context.Update(student);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!StudentExists(student.Serial))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
 
-            return RedirectToAction("__init__", "Students", new {sid = _getCurrentlyLoggedInUser()});
+                    return RedirectToAction("__init__", "Students", new {sid = _getCurrentlyLoggedInUser()});
+                }
+                default:
+                    await __init__(currentlyLoggedInUser);
+                    break;
+            }
+            return RedirectToAction("Logout", "Account");
         }
 
         public async Task<IActionResult>__classrooms___(string sid)
         {
-            if (_getCurrentlyLoggedInUser() == "" || _getCurrentlyLoggedInUser() == null)
+            var currentlyLoggedInUser = _getCurrentlyLoggedInUser();
+            var authCommand = __getAuthorizationCommand(sid);
+            
+            switch (authCommand)
             {
-                return RedirectToAction("LogIn", "Account");
+                case "Login":
+                    return RedirectToAction("LogIn", "Account");
+                case "Teacher":
+                    return RedirectToAction("__classrooms___", "Teachers", new { tid = currentlyLoggedInUser });
+                case "Self":
+                    await __classrooms___(currentlyLoggedInUser);
+                    break;
+                case "Continue":
+                {
+                    // For Layout asp-route-tid
+                    ViewBag.studentAccountId = sid;
+            
+                    var student = await _context.Students
+                        .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
+            
+                    return View(student);
+                }
+                default:
+                    await __init__(currentlyLoggedInUser);
+                    break;
             }
-            
-            if (_getAccountRoleFromUserId(sid) != "Student")
-            {
-                return RedirectToAction("__init__", "Teachers", new { tid = _getCurrentlyLoggedInUser() });
-            }
-
-            if (sid != _getCurrentlyLoggedInUser() || sid == null || Regex.Replace(sid, @"\s+", "") == "")
-            {
-                await __init__(_getCurrentlyLoggedInUser());
-            }
-            
-            // For Layout asp-route-tid
-            ViewBag.studentAccountId = sid;
-            
-            var student = await _context.Students
-                .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
-            
-            return View(student);
+            return RedirectToAction("Logout", "Account");
         }
         
         [HttpPost]
         public async Task<JsonResult>__classrooms___(string accessCode, string sid)
         {
-            try
-            {
-                var classroom = await _context.Classrooms
-                    .FirstOrDefaultAsync(cls => cls.AccessCode == accessCode);
-                var student = await _context.Students
-                    .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
+            var authCommand = __getAuthorizationCommand(sid);
 
-                if (classroom == null)
-                {
-                    return Json("codeinvalid");
-                }
-
-                //return Json(student);
-                if (student.CollegeId == null || student.HscBatch == null ||
-                    student.Shift == null)
-                {
-                    return Json("fill-up Profile");
-                }
-                
-                var enrollment = new StudentEnrollment
-                {
-                    ClassroomId = classroom.ClassroomId,
-                    StudentSerial = student.Serial,
-                    EnrollmentDateTime = DateTime.Now.Date
-                    
-                };
-                _context.Add(enrollment);
-                await _context.SaveChangesAsync();
-                return Json("success");
-            }
-            catch (Exception e)
+            switch (authCommand)
             {
-                return Json("failed");
-                
+                case "Login":
+                    return Json("failed");
+                case "Teacher":
+                    return Json("failed");
+                case "Self":
+                    return Json("failed");
+                case "Continue":
+                {
+                    try
+                    {
+                        var classroom = await _context.Classrooms
+                            .FirstOrDefaultAsync(cls => cls.AccessCode == accessCode);
+                        var student = await _context.Students
+                            .SingleOrDefaultAsync(stu => stu.Account.UserId == sid);
+
+                        if (classroom == null)
+                        {
+                            return Json("codeinvalid");
+                        }
+
+                        var enrollment = new StudentEnrollment
+                        {
+                            ClassroomId = classroom.ClassroomId,
+                            StudentSerial = student.Serial,
+                            EnrollmentDateTime = DateTime.Now.Date
+
+                        };
+                        _context.Add(enrollment);
+                        await _context.SaveChangesAsync();
+                        return Json("success");
+                    }
+                    catch (Exception)
+                    {
+                        return Json("failed");
+                    }
+                }
+                default:
+                    return Json("failed");
             }
-            
         }
         
         public async Task<JsonResult>__getClassRoom___(string sid)
         {
-            var classroomsOfStudent =
-                await _context.StudentEnrollments
-                    .Include(c => c.Student)
-                    .Include(c => c.Student.Account)
-                    .Include(c => c.Classroom)
-                    .Include(c => c.Classroom.Teacher.Account)
-                    .Where(stu => stu.Student.Account.UserId == sid)
-                    .ToListAsync();
+            var authCommand = __getAuthorizationCommand(sid);
+
+            switch (authCommand)
+            {
+                case "Login":
+                    return Json("failed");
+                case "Teacher":
+                    return Json("failed");
+                case "Self":
+                    return Json("failed");
+                case "Continue":
+                {
+                    var classroomsOfStudent =
+                        await _context.StudentEnrollments
+                            .Include(c => c.Student)
+                            .Include(c => c.Student.Account)
+                            .Include(c => c.Classroom)
+                            .Include(c => c.Classroom.Teacher.Account)
+                            .Where(stu => stu.Student.Account.UserId == sid)
+                            .ToListAsync();
             
                         
-            return Json(classroomsOfStudent);
-        }
-        
-        private bool StudentExists(int id)
-        {
-            return _context.Students.Any(e => e.Serial == id);
+                    return Json(classroomsOfStudent);
+                }
+                default:
+                    return Json("failed");
+            }
         }
     }
 }
--- src/wwwroot/app/src/scripts/__ClassroomJoin.js
+++ src/wwwroot/app/src/scripts/__ClassroomJoin.js
@@ -1,16 +1,12 @@
 var loggedInSid = $('#loggedInSid').val();
 
 var colorArray = [
-    "red darken-1", "teal darken-1", "indigo darken-1", "blue accent-3",
-    "purple darken-2", "pink darken-2", "red darken-2", "blue lighten-2",
-    "indigo", "deep-purple", "blue accent-1", "deep-purple accent-3",
-    "cyan lighten-3", "light-blue lighten-1", "cyan", "cyan accent-3",
-    "green", "light-green", "lime", "orange", "blue-grey lighten-1"
+    "red darken-1", "teal darken-1", "indigo darken-1", "blue-grey darken-1",
+    "purple darken-3", "blue lighten-1", "blue accent-2", "light-blue"
 ];
 
 $(document).ready(function () {
     getClassRoom();
-    
 $("#classroomForm").submit(function (e) {
     e.preventDefault();
     var AccessCode = $('#AccessCode').val();
@@ -18,21 +14,15 @@ $("#classroomForm").submit(function (e) {
     console.log("access code: "+AccessCode);
 
     $.post('', {accessCode : AccessCode, sid : studentId}, function (responseData) {
-        
-        //console.log(responseData);
         if(responseData === "failed"){
-            showMaterialToast("Invalid Request", "red darken-1" );
+            showMaterialToast("Invalid Request", "red darken-1 rounded" );
             $('#AccessCode').val("");
-        }
-        else if(responseData === "fill-up Profile") {
-            showMaterialToast("Full Profile Fill-up First", "amber darken-3");
-        }
-        else if(responseData === "codeinvalid"){
-            showMaterialToast("Enter a valid classroom code", "amber darken-3");
+        }else if(responseData === "codeinvalid"){
+            showMaterialToast("Enter a valid classroom code", "amber darken-3 rounded");
             $('#AccessCode').val("");
         }
         else if(responseData === "success"){
-            showMaterialToast("Classroom join Successfully", "teal darken-1");
+            showMaterialToast("Classroom join Successfully", "teal darken-1 rounded");
 
             $('#AccessCode').val("");
             getClassRoom();
@@ -65,31 +55,31 @@ function getClassRoom() {
 
             classrooms.html(modalToJoinNewClassroom);
         }else{
-
             var cardsForEachClassrooms = "<p class=\"center-align\"><a class=\"waves-effect waves-light btn-small materialize-indigo modal-trigger\" href=\"#joinClassroomModal\"><i class=\"material-icons left\">add</i>Join A New One</a></p>";
             cardsForEachClassrooms += "<input id=\"loggedInSid\" hidden value=\""+res[0]["student"]["account"]["userId"]+"\" type=\"text\"/>";
             cardsForEachClassrooms += "<div class=\"row\">"
 
             for (var i = res.length - 1; i >= 0; i--){
-                var randomIndex = Math.floor(Math.random() * 21);
+                var randomIndex = Math.floor(Math.random() * 8);
 
-                var eachClassroomCard  = "<div class=\"col s12 l3\">\n" +
-                    "                <div id=\"profile-card\" class=\"card\">\n" +
-                    "                    <div class=\"card-image waves-effect waves-block waves-light\">\n" +
-                    "                        <img class=\"activator\" src=\"https://img.freepik.com/free-vector/vector-illustration-mountain-landscape_1441-72.jpg?size=338&ext=jpg\" alt=\"user bg\" />\n" +
-                    "                    </div>\n" +
-                    "                    <div class=\"card-content\">\n" +
-                    "                        <img src=\"https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/avatar/avatar-7.png\" alt=\"\" class=\"circle responsive-img activator card-profile-image cyan lighten-1 padding-2\" />\n" +
-                    "                        <a target='_blank' class=\"btn-floating activator btn-move-up waves-effect waves-light "+colorArray[randomIndex]+" z-depth-4 right\" href=\"/Classrooms/__student__/"+loggedInSid+"/"+res[i]["classroom"]["classroomId"]+"\">" +
-                    "                            <i class=\"material-icons\">send</i>\n" +
-                    "                        </a>\n" +
-                    "                        <h5 class=\"card-title activator grey-text text-darken-4\">"+res[i]["classroom"]["classroomTitle"]+"</h5>\n" +
-                    "                        <p>Teacher Name : "+res[i]["classroom"]["teacher"]["account"]["userName"]+"</p>\n" +
-                    "                    </div>\n" +
-                    "                </div>\n" +
-                    "            </div>";
 
-                cardsForEachClassrooms += eachClassroomCard;
+            var eachClassroomCard = "<div class=\"col s12 l3\">\n" +
+                    " <div class=\"card\">\n" +
+                    " <div class=\"card-image waves-effect waves-block waves-light\">\n" +
+                    " <img class=\"activator\" src=\"https://img.freepik.com/free-vector/vector-illustration-mountain-landscape_1441-72.jpg?size=338&ext=jpg\" alt=\"user bg\" />\n" +
+                    " </div>\n" +
+                    " <div class=\"card-content\ "+colorArray[randomIndex]+" white-text\">\n" +
+                    /*" <img src=\"https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/avatar/avatar-7.png\" alt=\"\" class=\"circle responsive-img activator card-profile-image cyan lighten-1 padding-2\" />\n"+*/
+                    " <a class=\"btn-floating activator btn-move-up waves-effect waves-light blue darken-3 z-depth-4 right\" href=\"/Classrooms/__student__/"+loggedInSid+"/"+res[i]["classroom"]["classroomId"]+"\">\n" +
+                    " <i class=\"material-icons\ "+colorArray[randomIndex]+" white-text\">send</i>" +
+                    " </a>\n" +
+                    " <h1 class=\"card-title\">"+res[i]["classroom"]["classroomTitle"]+"</h1>\n" +
+                    " <p class=\"card-title\">"+res[i]["classroom"]["teacher"]["account"]["userName"]+"</p>\n" +
+                    " </div>\n" +
+                    " </div>\n" +
+                    " </div>";
+                
+               cardsForEachClassrooms += eachClassroomCard;
             }
             cardsForEachClassrooms += "</div>";
 
