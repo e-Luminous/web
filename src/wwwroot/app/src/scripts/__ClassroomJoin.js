6b406eb01396b4b0f847d846b6bd4f1a0a1bb67b
jinnatul <morolswediu@gmail.com>
2019-11-09 13:14:38 +0600

    fixed student classroom dynamic cards design with classroom info

--- src/wwwroot/app/src/scripts/__ClassroomJoin.js
+++ src/wwwroot/app/src/scripts/__ClassroomJoin.js
@@ -1,12 +1,16 @@
 var loggedInSid = $('#loggedInSid').val();
 
 var colorArray = [
-    "red darken-1", "teal darken-1", "indigo darken-1", "blue-grey darken-1",
-    "purple darken-3", "blue lighten-1", "blue accent-2", "light-blue"
+    "red darken-1", "teal darken-1", "indigo darken-1", "blue accent-3",
+    "purple darken-2", "pink darken-2", "red darken-2", "blue lighten-2",
+    "indigo", "deep-purple", "blue accent-1", "deep-purple accent-3",
+    "cyan lighten-3", "light-blue lighten-1", "cyan", "cyan accent-3",
+    "green", "light-green", "lime", "orange", "blue-grey lighten-1"
 ];
 
 $(document).ready(function () {
     getClassRoom();
+    
 $("#classroomForm").submit(function (e) {
     e.preventDefault();
     var AccessCode = $('#AccessCode').val();
@@ -55,31 +59,31 @@ function getClassRoom() {
 
             classrooms.html(modalToJoinNewClassroom);
         }else{
+
             var cardsForEachClassrooms = "<p class=\"center-align\"><a class=\"waves-effect waves-light btn-small materialize-indigo modal-trigger\" href=\"#joinClassroomModal\"><i class=\"material-icons left\">add</i>Join A New One</a></p>";
             cardsForEachClassrooms += "<input id=\"loggedInSid\" hidden value=\""+res[0]["student"]["account"]["userId"]+"\" type=\"text\"/>";
             cardsForEachClassrooms += "<div class=\"row\">"
 
             for (var i = res.length - 1; i >= 0; i--){
-                var randomIndex = Math.floor(Math.random() * 8);
+                var randomIndex = Math.floor(Math.random() * 21);
 
+                var eachClassroomCard  = "<div class=\"col s12 l3\">\n" +
+                    "                <div id=\"profile-card\" class=\"card\">\n" +
+                    "                    <div class=\"card-image waves-effect waves-block waves-light\">\n" +
+                    "                        <img class=\"activator\" src=\"https://img.freepik.com/free-vector/vector-illustration-mountain-landscape_1441-72.jpg?size=338&ext=jpg\" alt=\"user bg\" />\n" +
+                    "                    </div>\n" +
+                    "                    <div class=\"card-content\">\n" +
+                    "                        <img src=\"https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/avatar/avatar-7.png\" alt=\"\" class=\"circle responsive-img activator card-profile-image cyan lighten-1 padding-2\" />\n" +
+                    "                        <a target='_blank' class=\"btn-floating activator btn-move-up waves-effect waves-light "+colorArray[randomIndex]+" z-depth-4 right\" href=\"/Classrooms/__student__/"+loggedInSid+"/"+res[i]["classroom"]["classroomId"]+"\">" +
+                    "                            <i class=\"material-icons\">send</i>\n" +
+                    "                        </a>\n" +
+                    "                        <h5 class=\"card-title activator grey-text text-darken-4\">"+res[i]["classroom"]["classroomTitle"]+"</h5>\n" +
+                    "                        <p>Teacher Name : "+res[i]["classroom"]["teacher"]["account"]["userName"]+"</p>\n" +
+                    "                    </div>\n" +
+                    "                </div>\n" +
+                    "            </div>";
 
-            var eachClassroomCard = "<div class=\"col s12 l3\">\n" +
-                    " <div class=\"card\">\n" +
-                    " <div class=\"card-image waves-effect waves-block waves-light\">\n" +
-                    " <img class=\"activator\" src=\"https://img.freepik.com/free-vector/vector-illustration-mountain-landscape_1441-72.jpg?size=338&ext=jpg\" alt=\"user bg\" />\n" +
-                    " </div>\n" +
-                    " <div class=\"card-content\ "+colorArray[randomIndex]+" white-text\">\n" +
-                    /*" <img src=\"https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/avatar/avatar-7.png\" alt=\"\" class=\"circle responsive-img activator card-profile-image cyan lighten-1 padding-2\" />\n"+*/
-                    " <a class=\"btn-floating activator btn-move-up waves-effect waves-light blue darken-3 z-depth-4 right\" href=\"/Classrooms/__student__/"+loggedInSid+"/"+res[i]["classroom"]["classroomId"]+"\">\n" +
-                    " <i class=\"material-icons\ "+colorArray[randomIndex]+" white-text\">send</i>" +
-                    " </a>\n" +
-                    " <h1 class=\"card-title\">"+res[i]["classroom"]["classroomTitle"]+"</h1>\n" +
-                    " <p class=\"card-title\">"+res[i]["classroom"]["teacher"]["account"]["userName"]+"</p>\n" +
-                    " </div>\n" +
-                    " </div>\n" +
-                    " </div>";
-                
-               cardsForEachClassrooms += eachClassroomCard;
+                cardsForEachClassrooms += eachClassroomCard;
             }
             cardsForEachClassrooms += "</div>";
 
