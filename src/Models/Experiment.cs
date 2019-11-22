009ebffb5a5d05eba2d9bbb7c737a4166dafc44d
shunjid <shunjid1862@gmail.com>
2019-11-22 23:54:58 +0600

    restored db with Migrations

--- src/Models/Experiment.cs
+++ src/Models/Experiment.cs
@@ -20,6 +20,9 @@ namespace src.Models
         [DataType(DataType.Html)]
         public string ExperimentalTableBodyMarkUp { get; set; }
         
+        [DataType(DataType.MultilineText)]
+        public string ExperimentalTableJsonStructure { get; set; }
+        
         //Relations 
         public Course Course { get; set; }
         public List<Submission> Submissions { get; set; }
