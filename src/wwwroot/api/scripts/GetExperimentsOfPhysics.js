72bddb76ec1cc50bb47ce32ff6b19e91c60b7c3a
jinnatul <morolswediu@gmail.com>
2019-11-25 17:33:53 +0600

    implement experiment 07

--- src/wwwroot/api/scripts/GetExperimentsOfPhysics.js
+++ src/wwwroot/api/scripts/GetExperimentsOfPhysics.js
@@ -9,69 +9,134 @@ $(document).ready(function () {
     });
 });
 
+
+/* Exp Phy 01 */
 function InitiatePhy01(experiment, submission) {
     var exp01 = JSON.parse(experiment["experimentalTableJsonStructure"]);
-    //var submissionAPI = submission.
+    var submissionAPI = JSON.parse(submission["apiData"]);
+
     var exp01Table = $('#exp01Table');
-    var columns = setHeaders(exp01, exp01Table);
-    console.log(exp01.length);
-    console.log(columns);
-    setBody(columns, exp01Table, exp01);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp01, exp01Table);
+        setBody(columns, exp01Table, exp01);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp01Table);
+        setBody(columns, exp01Table, submissionAPI);
+    }
+    
     $('#exp01Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy02(experiment) {
+/* Exp Phy 02 */
+function InitiatePhy02(experiment, submission) {
     var exp02 = JSON.parse(experiment["experimentalTableJsonStructure"]);
+    var submissionAPI = JSON.parse(submission["apiData"]);
+
     var exp02Table = $('#exp02Table');
-    var columns = setHeaders(exp02, exp02Table);
-    setBody(columns, exp02Table, exp02);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp02, exp02Table);
+        setBody(columns, exp02Table, exp02);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp02Table);
+        setBody(columns, exp02Table, submissionAPI);
+    }
+
     $('#exp02Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy03(experiment) {
+/* Exp Phy 03 */
+function InitiatePhy03(experiment, submission) {
     var exp03 = JSON.parse(experiment["experimentalTableJsonStructure"]);
-    
-    console.log(exp03);
-    //console.log("length exp obj : " + exp03.length);
-
+    var submissionAPI = JSON.parse(submission["apiData"]);
+ 
     var exp03Table = $('#exp03Table');
-    var columns = setHeaders(exp03, exp03Table);
-    console.log("columns count Exp3: " + columns);
-    setBody(columns, exp03Table, exp03);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp03, exp03Table);
+        setBody(columns, exp03Table, exp03);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp03Table);
+        setBody(columns, exp03Table, submissionAPI);
+    }
+    
     $('#exp03Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy04(experiment) {
+/* Exp Phy 04 */
+function InitiatePhy04(experiment, submission) {
     var exp04 = JSON.parse(experiment["experimentalTableJsonStructure"]);
+    var submissionAPI = JSON.parse(submission["apiData"]);
+
     var exp04Table = $('#exp04Table');
-    var columns = setHeaders(exp04, exp04Table);
-    setBody(columns, exp04Table, exp04);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp04, exp04Table);
+        setBody(columns, exp04Table, exp04);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp04Table);
+        setBody(columns, exp04Table, submissionAPI);
+    }
+    
     $('#exp04Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy05(experiment) {
+/* Exp Phy 05 */
+function InitiatePhy05(experiment, submission) {
     var exp05 = JSON.parse(experiment["experimentalTableJsonStructure"]);
+    var submissionAPI = JSON.parse(submission["apiData"]);
+ 
     var exp05Table = $('#exp05Table');
-    var columns = setHeaders(exp05, exp05Table);
-    setBody(columns, exp05Table, exp05);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp05, exp05Table);
+        setBody(columns, exp05Table, exp05);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp05Table);
+        setBody(columns, exp05Table, submissionAPI);
+    }
+    
     $('#exp05Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy06(experiment) {
+
+/* Exp Phy 06 */
+function InitiatePhy06(experiment, submission) {
     var exp06 = JSON.parse(experiment["experimentalTableJsonStructure"]);
+    var submissionAPI = JSON.parse(submission["apiData"]);
+    
     var exp06Table = $('#exp06Table');
-    var columns = setHeaders(exp06, exp06Table);
-    setBody(columns, exp06Table, exp06);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp06, exp06Table);
+        setBody(columns, exp06Table, exp06);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp06Table);
+        setBody(columns, exp06Table, submissionAPI);
+    }
+
     $('#exp06Table td').attr('contenteditable','true');
 }
 
-function InitiatePhy07(experiment) {
+/* Exp Phy 07 */
+function InitiatePhy07(experiment, submission) {
     var exp07 = JSON.parse(experiment["experimentalTableJsonStructure"]);
+    var submissionAPI = JSON.parse(submission["apiData"]);
+    
     var exp07Table = $('#exp07Table');
-    var columns = setHeaders(exp07, exp07Table);
-    setBody(columns, exp07Table, exp07);
+    if(submissionAPI == null){
+        var columns = setHeaders(exp07, exp07Table);
+        setBody(columns, exp07Table, exp07);
+    }
+    else {
+        var columns = setHeaders(submissionAPI, exp07Table);
+        setBody(columns, exp07Table, submissionAPI);
+    }
+
     $('#exp07Table td').attr('contenteditable','true');
 }
+
 
 
 /* Set All Table Header */
@@ -111,22 +176,39 @@ function setBody(columns, expTable, exp) {
 
                 console.log("Nth radio checked : " + val);
 
-                if(val == 1){
-                    radioString += '<label><input type="radio" name="group1" checked/> <span>বায়ু মন্ডলের চাপ</span> </label>' + '<br>';
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
+                if(val == 0){
+                    radioString += '<label><input type="radio" name="group'+i+'" checked/> <span>বায়ু মন্ডলের চাপ</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
+                }
+                else if(val == 1){
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপ</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'" checked/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
                 }
                 else if(val == 2){
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপ</span> </label>' + '<br>';
-                    radioString += '<label><input type="radio" name="group1" checked/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
-                }
-                else if(val == 3){
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপ</span> </label>'+'<br>';
-                    radioString += '<label><input type="radio" name="group1"/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
-                    radioString += '<label><input type="radio" name="group1" checked/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপ</span> </label>'+'<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'"/> <span>বায়ু মন্ডলের চাপের বেশী</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="group'+i+'" checked/> <span>বায়ু মন্ডলের চাপের কম</span> </label>';
                 }
 
+                console.log(radioString);
+                row.append($('<tr/>').html(radioString));
+                continue;
+            }
+
+            if(columns[0] === "ব্যবস্থা" && colIndex == 0) {
+                var radioString = ' ';
+
+                if(val == 0){
+                    radioString += '<label><input type="radio" name="typeOf'+i+'" checked/> <span>লম্বিক ব্যবস্থা</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="typeOf'+i+'"/> <span>আড়াআড়ি ব্যবস্থা</span> </label>' + '<br>';
+                }
+                else if(val == 1){
+                    radioString += '<label><input type="radio" name="typeOf'+i+'" /> <span>লম্বিক ব্যবস্থা</span> </label>' + '<br>';
+                    radioString += '<label><input type="radio" name="typeOf'+i+'" checked/> <span>আড়াআড়ি ব্যবস্থা</span> </label>' + '<br>';
+                }
+                
                 row.append($('<tr/>').html(radioString));
                 continue;
             }
