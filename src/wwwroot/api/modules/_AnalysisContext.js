2d7225fe0d6c7e9148f02a25a0d7a3e7888f008c
shunjid <shunjid1862@gmail.com>
2019-12-08 01:17:46 +0600

    moved Euclidean to seperate JS File

--- src/wwwroot/api/modules/_AnalysisContext.js
+++ src/wwwroot/api/modules/_AnalysisContext.js
@@ -2,51 +2,118 @@ $(function () {
     let submissions = [];
     let studentId = $('#StudentIdFromViewBag').val();
     let classroomId = $('#ClassroomIdFromViewBag').val();
+
     getTableData(studentId, classroomId, submissions);
-    $('.conPhy').on("click", function () {
-        let btnClickedId = this.id;
-        let nearestExperimentTableId = btnClickedId.replace('convert', 'exp');
+    
+    $('.btnMLPhy').on("click", function () {
+        let btnMLClickedId = this.id;
+        let nearestExperimentTableId = btnMLClickedId.replace('btnML', 'exp');
         let jQFormat = '#' + nearestExperimentTableId;
         let table = convertTable(jQFormat, {});
-        let indexInSubmission = submissions.findIndex(p => p["experiment"]["scriptFunctionToEvaluateExperiment"] == nearestExperimentTableId);
+        let indexInSubmission = submissions.findIndex(p => p["experiment"]["scriptFunctionToEvaluateExperiment"] === nearestExperimentTableId);
         let experimentBaseStructure = JSON.parse(submissions[indexInSubmission]["experiment"]["experimentalTableJsonStructure"])[0];
         let keysWithRowSpans = [];
         let maxLengthOfAnArray = 0;
+
         for (let key in experimentBaseStructure) {
             if (experimentBaseStructure.hasOwnProperty(key) && Array.isArray(experimentBaseStructure[key])) {
                 keysWithRowSpans.push(key);
                 maxLengthOfAnArray = Math.max(maxLengthOfAnArray, experimentBaseStructure[key].length);
             }
         }
+
         let tempArrayOfObject = [];
         for (let x = 0; x < table.length; x++) {
             if (x % maxLengthOfAnArray === 0) {
                 for (let y = 0; y < keysWithRowSpans.length; y++) {
                     let tmpObj = {};
-                    if (tmpObj.hasOwnProperty(keysWithRowSpans[y]) == false) {
+                    if (tmpObj.hasOwnProperty(keysWithRowSpans[y]) === false) {
                         tmpObj[keysWithRowSpans[y]] = [table[x][keysWithRowSpans[y]]];
                     }
                     tempArrayOfObject.push(tmpObj);
                 }
             }
         }
-        let red = mapReduce(table, keysWithRowSpans, maxLengthOfAnArray, tempArrayOfObject);
-        let submissionID = submissions[indexInSubmission]["submissionId"];
-        let SubmitStatus = submissions[indexInSubmission]["status"];
-        //console.log(submissions);
-        $.post('/Classrooms/PostPhysicsSubmissionOfTheStudent', {
-            SubmitStatus: SubmitStatus,
-            postJsonPhy: JSON.stringify(red),
-            submissionID: submissionID
-        }, function (responseData) {
-            if (responseData === "success") {
-                showMaterialToast("Date saved successfully", "green darken-1");
-            }
-        }).then(function () {
-            showMaterialToast("Keep continuing", "blue darken-3");
-        });
+
+        let standardJsonForMachineLearning = JSON.parse(submissions[indexInSubmission]["experiment"]["standardJsonForMachineLearning"]);
+        let reduceJson = mapReduce(table, keysWithRowSpans, maxLengthOfAnArray, tempArrayOfObject);
+        let reduceJsonLength = reduceJson.length;
+
+        let headerTable = Object.keys(standardJsonForMachineLearning[0]);
+
+        let minimumDistance = [];
+        let maximumDistance = [];
+        for (let posReduce = 0; posReduce < reduceJsonLength; posReduce++){
+            Euclidean_Distance(minimumDistance,maximumDistance,standardJsonForMachineLearning,headerTable,reduceJson,posReduce)
+        }
+        constructAnalyticalModel(minimumDistance, maximumDistance, submissions[indexInSubmission]);
     });
+
 });
+
+function constructAnalyticalModel(minDistance, maxDistance, submissionRequested) {
+    $('#analyticalModal').modal('open');
+    $('#analyticalModalHeader').text(submissionRequested["experiment"]["experimentName"]);
+    
+    minDistance.forEach(function (element) {
+       Object.keys(element).forEach(function (key) {
+           console.log(key, element[key]);
+       }) 
+    });
+    
+    console.log(minDistance);
+    console.log(maxDistance);
+}
+
+
+function Euclidean_Distance(minimumDistance,maximumDistance,standardJsonForMachineLearning,headerTable,reduceJson,posReduce) {
+    let headerLength = headerTable.length;
+    let standardJsonLength = standardJsonForMachineLearning.length;
+
+    let miniBest = new Array(headerLength).fill(10000);
+    let maxiBest = new Array(headerLength).fill(0);
+    
+    for(let posStandard = 0; posStandard < standardJsonLength; posStandard++){
+        for(let posHeader = 0; posHeader < headerLength; posHeader++){
+            let keyName = headerTable[posHeader];
+            if(keyName === "ব্যবস্থা" || keyName === "চাপ") continue;
+            let objStandard = standardJsonForMachineLearning[posStandard][keyName];
+            let objReduce = reduceJson[posReduce][keyName];
+
+            if(objReduce.length > 0) {
+                if(posStandard === 0){
+                    miniBest[posHeader] = new Array(objReduce.length).fill(10000);
+                    maxiBest[posHeader] = new Array(objReduce.length).fill(0);
+                }
+                for(let subpos = 0; subpos < objReduce.length; subpos++) {
+                    let tempDistance = Math.abs(objStandard[subpos] - objReduce[subpos]);
+                    miniBest[posHeader][subpos] = Math.min(miniBest[posHeader][subpos], tempDistance);
+                    maxiBest[posHeader][subpos] = Math.max(maxiBest[posHeader][subpos], tempDistance);
+                }
+            }
+            else{
+                let tempDistance = Math.abs(standardJsonForMachineLearning[posStandard][headerTable[posHeader]]
+                    - reduceJson[posReduce][headerTable[posHeader]]);
+                miniBest[posHeader] = Math.min(miniBest[posHeader], tempDistance);
+                maxiBest[posHeader] = Math.max(maxiBest[posHeader], tempDistance);
+            }
+        }
+    }
+
+    let tempObjMinimum= {}, tempObjMaximum = {};
+    for(let posHeader = 0; posHeader < headerLength; posHeader++){
+        let keys = headerTable[posHeader];
+        if(keys === "ব্যবস্থা" || keys === "চাপ") continue;
+        let miniDistance = miniBest[posHeader];
+        let maxDistance = maxiBest[posHeader];
+        tempObjMinimum[keys] = miniDistance;
+        tempObjMaximum[keys] = maxDistance;
+    }
+    minimumDistance.push(tempObjMinimum);
+    maximumDistance.push(tempObjMaximum);
+}
+
+
 function getTableData(studentId, classroomId, submissions) {
     $.get('/Classrooms/GetPhysicsSubmissionOfTheStudent', {
         studentId: studentId,
@@ -59,32 +126,40 @@ function getTableData(studentId, classroomId, submissions) {
         showMaterialToast("Ready to explore", "grey darken-3");
     });
 }
+
 function mapReduce(data, keysWithRowSpans, maxRowSpans, initArrayOfObjects) {
     let spanLength = keysWithRowSpans.length;
     let initAOBLength = initArrayOfObjects.length;
+
     if (maxRowSpans === 0) {
         return data;
-    }
-    else {
+    } else {
         let traverseStart = 0;
         let traverseEnd = spanLength - 1;
+
         return data.reduce((r, e, i, a) => {
             if (i % maxRowSpans === 0) {
                 const next = [];
+
                 for (let x = 1; x < maxRowSpans; x++) {
                     next.push(a[i + x]);
                 }
-                let obj = Object.assign({}, e);
+
+                let obj = {...e};
                 let yy = -1;
+
                 for (let xx = traverseStart; xx <= traverseEnd; xx++) {
                     const tempObject = initArrayOfObjects[xx];
-                    obj = Object.assign(Object.assign({}, obj), tempObject);
+                    obj = {...obj, ...tempObject};
                     yy = xx;
                 }
+
                 if (yy !== initAOBLength - 1) {
                     traverseStart += spanLength;
                     traverseEnd += spanLength;
                 }
+
+
                 for (let index = 0; index < next.length; index++) {
                     if (next[index]) {
                         for (let spanKey = 0; spanKey < spanLength; spanKey++) {
@@ -92,12 +167,14 @@ function mapReduce(data, keysWithRowSpans, maxRowSpans, initArrayOfObjects) {
                         }
                     }
                 }
+
                 r.push(obj);
             }
             return r;
         }, []);
     }
 }
+
 function convertTable(tableId, opts) {
     let defaults = {
         ignoreColumns: [],
@@ -106,18 +183,23 @@ function convertTable(tableId, opts) {
         headings: null,
         allowHTML: true
     };
+
     opts = $.extend(defaults, opts);
+
     let notNull = function (value) {
         return value !== undefined && value !== null;
     };
+
     let ignoredColumn = function (index) {
         if (notNull(opts.onlyColumns)) {
             return $.inArray(index, opts.onlyColumns) === -1;
         }
         return $.inArray(index, opts.ignoreColumns) !== -1;
     };
+
     let arraysToHash = function (keys, values) {
-        let result = {}, index = 0;
+        let result = {},
+            index = 0;
         $.each(values, function (i, value) {
             // when ignoring columns, the header option still starts
             // with the first defined column
@@ -128,35 +210,36 @@ function convertTable(tableId, opts) {
         });
         return result;
     };
+
     let cellValues = function (cellIndex, cell) {
         let value, result;
         if (!ignoredColumn(cellIndex)) {
             let override = $(cell).data('override');
+
             if (opts.allowHTML) {
                 let cellHTML = $(cell).html();
+
                 if (cellHTML.includes("বায়ু মন্ডলের চাপ")) {
                     let groupStr = cellHTML.indexOf("group");
                     let groupNum = cellHTML.charAt(groupStr + 5);
                     let groupName = 'group' + groupNum.toString();
                     value = $("input[name='" + groupName + "']:checked").val();
-                }
-                else if (cellHTML.includes("লম্বিক ব্যবস্থা")) {
+                } else if (cellHTML.includes("লম্বিক ব্যবস্থা")) {
                     let typeStr = cellHTML.indexOf("typeOf");
                     let typeNum = cellHTML.charAt(typeStr + 6);
                     let typeName = 'typeOf' + typeNum.toString();
                     value = $("input[name='" + typeName + "']:checked").val();
-                }
-                else {
+                } else {
                     value = $.trim($(cell).html());
                 }
-            }
-            else {
+            } else {
                 value = $.trim($(cell).text());
             }
             result = notNull(override) ? override : value;
         }
         return result;
     };
+
     let rowValues = function (row) {
         let result = [];
         $(row).children('td,th').each(function (cellIndex, cell) {
@@ -166,12 +249,18 @@ function convertTable(tableId, opts) {
         });
         return result;
     };
+
     let getHeadings = function (table) {
         let firstRow = table.find('tr:first').first();
         return notNull(opts.headings) ? opts.headings : rowValues(firstRow);
     };
+
     let construct = function (table, headings) {
-        let i, len, txt, $row, $cell, tmpArray = [], cellIndex = 0, result = [];
+        let i, len, txt, $row, $cell,
+            tmpArray = [],
+            cellIndex = 0,
+            result = [];
+
         table.children('tr').each(function (rowIndex, row) {
             if (rowIndex > 0 || notNull(opts.headings)) {
                 $row = $(row);
@@ -183,6 +272,7 @@ function convertTable(tableId, opts) {
                     $row.children().each(function () {
                         if (!ignoredColumn(cellIndex)) {
                             $cell = $(this);
+
                             // process rowspans
                             if ($cell.filter('[rowspan]').length) {
                                 len = parseInt($cell.attr('rowspan'), 10) - 1;
@@ -220,6 +310,7 @@ function convertTable(tableId, opts) {
                 }
             }
         });
+
         $.each(tmpArray, function (i, row) {
             if (notNull(row)) {
                 // filter table inputs to number
@@ -230,20 +321,23 @@ function convertTable(tableId, opts) {
         });
         return result;
     };
+
     // Run
     let headings = getHeadings($(tableId));
     return construct($(tableId), headings);
 }
+
+
 function convertArrayToFloat(arr) {
     return arr.map(function (x) {
         //return isNumber(x) ? parseFloat(x) : 0;
         return isNaN(x) ? 0 : parseFloat(x);
     });
 }
+
 function showMaterialToast(data, style) {
     M.toast({
-        html: data,
-        classes: style
+        html : data,
+        classes : style
     });
 }
-//# sourceMappingURL=_PostPhysicsExperiments.js.map
